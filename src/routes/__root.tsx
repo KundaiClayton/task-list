171d0d49e58a8c272f421268d099c21f2346b6f5
KundaiClay <kundai@mohara.co>
2024-12-15 01:44:31 +0200

    feat: lazy load TanStackRouterDevtools in development environment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,20 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { ThemeProvider } from '@mui/material/styles'
 import {  Container, CssBaseline } from '@mui/material'
 import { theme } from '@/theme/theme' 
 import { Header } from '@/components/Header'
+import React from 'react'
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+         
+        })),
+      )
 
 export const Route = createRootRoute({
   component: () => (
