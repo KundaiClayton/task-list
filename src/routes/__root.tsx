1f3f980ccb26c2649ccbbf5c0327cb4d849638b0
KundaiClay <kundai@mohara.co>
2024-12-14 19:00:47 +0200

    feat: add Material UI dependencies and theme configuration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,26 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { ThemeProvider } from '@mui/material/styles'
+import { AppBar, Toolbar, Typography, Container, CssBaseline } from '@mui/material'
+import { theme } from '@/theme/theme' 
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+              Task List
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 3 }}>
+        <Outlet />
+      </Container>
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 })
